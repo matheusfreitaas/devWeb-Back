45df518a4e44ae1a11b2c28a7192b9ba155f8f54
Matheus Freitas <matheusbfreitas@gmail.com>
2018-09-12 19:55:40 -0300

    Add body-parser package

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 const app = express();
@@ -19,16 +20,18 @@ app.get('/', function (req, res) {
     res.send('Hello World!')
 });
 
-/*app.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ a: 1 })
-});
+/*// faz o parse de requisições com o corpo do tipo application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// faz o parse de requisições com o corpo do tipo application/json
+app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
     res.header('Content-Type', 'application/json');
     next();  // sem o next, a chamada para aqui
 });
 
-app.get('/api/endpoint1', (req, res) => {
-    res.send(JSON.stringify({value: 1}));
-});*/
+app.post('/', function (req, res) {
+  // aqui estamos devolvendo ao cliente o corpo da requisição POST realizada pelo mesmo.
+  res.end(JSON.stringify(req.body, null, 2))
+}); */
